7c835c876674fa1eb4b70fe4622edf091cb40f33
Sarah Friedman <sarah.friedman@noaa.gov>
2024-06-13 15:34:23 -0700

    quick fix

--- code/00_download_data.R
+++ code/00_download_data.R
@@ -1,41 +1,41 @@
 ## Download data sets to local machine -------------------------------------------------------
+
+# RACEBASE tables to query
+locations <- c(
+  "RACE_DATA.V_CRUISES",
+  "RACE_DATA.RACE_SPECIES_CODES",
+
+  # biological edit data
+  "RACE_DATA.EDIT_CATCH_SPECIES",
+  "RACE_DATA.EDIT_CATCH_SAMPLES",
+  "RACE_DATA.EDIT_LENGTHS",
+  "RACE_DATA.EDIT_SPECIMENS",
+
+  # effort edit data
+  "RACE_DATA.EDIT_HAULS",
+  "RACE_DATA.EDIT_EVENTS",
+  "RACE_DATA.EDIT_HAUL_MEASUREMENTS"
+  #    "RACE_DATA.V_EXTRACT_FINAL_LENGTHS",
+)
+
+historical_data <- c(
+  # historical data
+  "RACEBASE.CATCH",
+  "RACEBASE.LENGTH",
+  "RACEBASE.HAUL",
+  "RACEBASE.SPECIMEN",
+  "RACEBASE.CRUISE",
+  "RACEBASE.CATCH"
+)
+
+if (!file.exists("data/oracle")) dir.create("data/oracle", recursive = TRUE)
+files <- tools::file_path_sans_ext(toupper(list.files("data/oracle")))
+if (!use_cached | !all(gsub("\\.", "-", historical_data) %in% files)) locations <- c(locations, historical_data)
+
 if (exists("channel")) {
-  # RACEBASE tables to query
-  locations <- c(
-    "RACE_DATA.V_CRUISES",
-    "RACE_DATA.RACE_SPECIES_CODES",
-
-    # biological edit data
-    "RACE_DATA.EDIT_CATCH_SPECIES",
-    "RACE_DATA.EDIT_CATCH_SAMPLES",
-    "RACE_DATA.EDIT_LENGTHS",
-    "RACE_DATA.EDIT_SPECIMENS",
-
-    # effort edit data
-    "RACE_DATA.EDIT_HAULS",
-    "RACE_DATA.EDIT_EVENTS",
-    "RACE_DATA.EDIT_HAUL_MEASUREMENTS",
-    #    "RACE_DATA.V_EXTRACT_FINAL_LENGTHS",
-  )
-  
-  historical_data <- c(
-    # historical data
-    "RACEBASE.CATCH",
-    "RACEBASE.LENGTH",
-    "RACEBASE.HAUL",
-    "RACEBASE.SPECIMEN",
-    "RACEBASE.CRUISE",
-    "RACEBASE.CATCH"
-  )
-  
-  if (!file.exists("data/oracle")) dir.create("data/oracle", recursive = TRUE)
-  files <- tools::file_path_sans_ext(toupper(list.files("data/oracle")))
-  if(!use_cached | !all(gsub("\\.", "-", historical_data) %in% files))  locations <- c(locations, historical_data)
-  
-
   # downloads tables in "locations"
   for (i in 1:length(locations)) {
-    print(paste0("Downloading data from ",locations[i]))
+    print(paste0("Downloading data from ", locations[i]))
     filename <- tolower(gsub("\\.", "-", locations[i]))
     a <- RODBC::sqlQuery(channel, paste0("SELECT * FROM ", locations[i]))
     readr::write_csv(
